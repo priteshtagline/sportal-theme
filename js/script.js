cfecfb41032f35d344c55272cebd8b6aba822334
Vivek Tagline <vivek.tagline@gmail.com>
2022-10-31 09:50:04 +0530

    Redesign of auth page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,42 @@
+$(document).ready(function () {
+  //  Sign in form
+  $("#do_not_account").click(function () {
+    $("#sign_in").addClass("d-none");
+    $("#sign_up").removeClass("d-none");
+  });
+
+  // sign up form
+  $("#already_member").click(function () {
+    $("#sign_in").removeClass("d-none");
+    $("#sign_up").addClass("d-none");
+  });
+
+  //forgotten link
+  $("#forgotten_password").click(function () {
+    $("#sign_in").addClass("d-none");
+    $("#forgotten_form").removeClass("d-none");
+  });
+
+  // back_Sign_in forgottent form
+  $("#back_signin").click(function () {
+    $("#forgotten_form").addClass("d-none");
+    $("#sign_in").removeClass("d-none");
+  });
+
+  // back_Sign_in change password
+  $("#password_to_signin").click(function () {
+    $("#change_password").addClass("d-none");
+    $("#sign_in").removeClass("d-none");
+  });
+
+  // Sidebar collapse
+  $("#side_nav_btn").on("click", function () {
+    $(".sidebar").toggleClass("active");
+    $("header").toggleClass("active");
+    $(".main-content").toggleClass("active");
+    $(this).toggleClass("active");
+  });
+});
 
 // Include html file
 function includeHTML() {
@@ -13,69 +52,40 @@ function includeHTML() {
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function () {
         if (this.readyState == 4) {
-          if (this.status == 200) { elmnt.innerHTML = this.responseText; }
-          if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
           /*remove the attribute, and call this function once more:*/
           elmnt.removeAttribute("w3-include-html");
           includeHTML();
         }
-      }
+      };
       xhttp.open("GET", file, true);
       xhttp.send();
       /*exit the function:*/
       return;
     }
   }
-};
+}
 
 includeHTML();
 
-  //  Sign in form 
-  $(document).ready(function () {
-    $("#do_not_account").click(function () {
-      $("#sign_in").addClass("d-none");
-      $("#sign_up").removeClass("d-none");
-    });
-  });
-
-  // sign up form
-  $(document).ready(function () {
-    $("#already_member").click(function () {
-      $("#sign_in").removeClass("d-none");
-      $("#sign_up").addClass("d-none");
-    });
-  });
-  //forgotten link
-  $(document).ready(function () {
-    $("#forgotten_password").click(function () {
-      $("#sign_in").addClass("d-none");
-      $("#forgotten_form").removeClass("d-none");
-    });
-  });
-  // back_Sign_in forgottent form
-  $(document).ready(function () {
-    $("#back_signin").click(function () {
-      $("#forgotten_form").addClass("d-none");
-      $("#sign_in").removeClass("d-none");
-    });
-  });
-  // back_Sign_in change password
-  $(document).ready(function () {
-    $("#password_to_signin").click(function () {
-      $("#change_password").addClass("d-none");
-      $("#sign_in").removeClass("d-none");
-    });
-  });
-
-// Sidebar collapse
-
-$(document).ready(function() {
-  $("#side_nav_btn").on("click", function() {
-    $(".sidebar").toggleClass("active");
-    $("header").toggleClass("active");
-    $(".main-content").toggleClass("active");
-    $(this).toggleClass("active");
-  });
-});
-
-
+// Image Upload Javascript
+("use strict");
+function dragNdrop(event) {
+  var fileName = URL.createObjectURL(event.target.files[0]);
+  var preview = document.getElementById("preview");
+  var previewImg = document.createElement("img");
+  previewImg.setAttribute("src", fileName);
+  preview.innerHTML = "";
+  preview.appendChild(previewImg);
+}
+function drag() {
+  document.getElementById("uploadFile").parentNode.className = "dragBox";
+}
+function drop() {
+  document.getElementById("uploadFile").parentNode.className = "dragBox";
+}
