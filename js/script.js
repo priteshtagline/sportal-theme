f511c5d36b76ef14fc75015dd604ede800dbc0f2
YashTagline <yashk.tagline@gmail.com>
2022-11-03 12:53:08 +0530

    SPOR-4 Setting module in progress

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
+
 $(document).ready(function () {
+     // sidebar profile 
+     $('.profile-dropdown').on('click', function () {
+      $('.features-list li a.active').removeClass('active');
+      $('.profile-dropdown').addClass('active')
+      $('.user-detail').on('click', 'li', function () {
+        $('.features-list li a.active').removeClass('active');
+        $('.profile-dropdown').addClass('active')
+      })
+    })
   //  Sign in form
   $("#do_not_account").click(function () {
     $("#sign_in").addClass("d-none");
@@ -38,40 +48,7 @@ $(document).ready(function () {
   });
 });
 
-// Include html file
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /*loop through a collection of all HTML elements:*/
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      /*make an HTTP request using the attribute value as the file name:*/
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function () {
-        if (this.readyState == 4) {
-          if (this.status == 200) {
-            elmnt.innerHTML = this.responseText;
-          }
-          if (this.status == 404) {
-            elmnt.innerHTML = "Page not found.";
-          }
-          /*remove the attribute, and call this function once more:*/
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
-      };
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /*exit the function:*/
-      return;
-    }
-  }
-}
 
-includeHTML();
 
 // Image Upload Javascript
 ("use strict");
