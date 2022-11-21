931bf78644d9832fad02bac33e153a15bc8a9ffa
YashTagline <yashk.tagline@gmail.com>
2022-11-21 11:40:03 +0530

    spor-8 bootstrap 4 and select2 updated

--- js/script.js
+++ js/script.js
@@ -1,55 +1,66 @@
-
 $(document).ready(function () {
-
   // sidebar resize
-  $(window).resize(function() {
+  $(window).resize(function () {
     if ($(window).width() <= 1024) {
-     $('.sidebar').addClass('active');
-     $('.main-content').addClass('active');
-     $('header').addClass('active');
+      $('.sidebar').addClass('active');
+      $('.main-content').addClass('active');
+      $('header').addClass('active');
     }
   });
 
   // sidebar - navtab
-  $("#settings-tab").on('click',function(){ 
+  $("#settings-tab").on('click', function () {
     $('.navbar-brand').html('Settings');
     $('#setting_nav_tab').show();
+    $('#nav_search_field').hide();
   });
-  $("#customers-tab").click(function(){
-    $('.navbar-brand').html('Customers');
-    $('#setting_nav_tab').hide(); 
+
+  $('#customer_tab').hide();
+  $("#customers-tab").click(function () {
+    $('.navbar-brand').html('Customers').show();
+    $('#customer_tab').show();
+    $('#setting_nav_tab').hide();
+    $('#customer_nav_list').show();
+    $('#nav_search_field').hide();
+  });
+  $("#schedule-tab").click(function () {
+    $('.navbar-brand').hide();
+    $('#customer_tab').show();
+    $('#customer_title').hide();
+    $('#nav_search_field').show();
+
   });
 
   // Setting_nav_tab
   $('#setting_nav_tab').hide();
-  $("#settings-tab").click(function(){
+  $("#settings-tab").click(function () {
     $('.navbar-brand').html('Settings');
-    $('#customer_title').hide(); 
+    $('#customer_title').hide();
   });
 
   // Customer_nav_tab
   $('#customer_subtab').hide();
   $('#nav-back-arrow').hide();
   $('#customer_subtab_content').hide();
-  $('#customer_show_more_btn').on('click',function(){
+  $('#customer_show_more_btn').on('click', function () {
     $('.navbar-brand').hide();
     $('#customer_title').hide();
     $('#customer_subtab').show();
     $('#customer_subtab_content').show();
-    $('#customer_main_module').hide();  
+    $('#customer_main_module').hide();
     $('#nav-back-arrow').show();
   })
-  $('#nav-back-arrow').on('click',function(){
+  $('#nav-back-arrow').on('click', function () {
     $('.navbar-brand').show();
     $('#customer_title').show();
     $('#customer_subtab').hide();
     $('#customer_subtab_content').hide();
-    $('#customer_main_module').show();  
+    $('#customer_main_module').show();
     $('#nav-back-arrow').hide();
   })
 
   // scheduletime for setting overview
-  $('.btn').on('click',function(){
+  $('.btn').on('click', function () {
     $(this).closest('li.schedule_time_list').toggleClass('edit');
   })
   $(".form_card .btn-reset").click(function () {
@@ -57,15 +68,15 @@ $(document).ready(function () {
   });
 
   // sidebar profile 
-  $('.sidebar .profile-dropdown').on('click', function(){
-  $('.features-list li a.active').removeClass('active');
-  $('.main-content .tab-content .tab-pane').removeClass('show active');
-  $('.sidebar .profile-dropdown').addClass('active')
-  $('.features-list li a').on('click', function(){
-    $('.sidebar .profile-dropdown').removeClass('active');
+  $('.sidebar .profile-dropdown').on('click', function () {
+    $('.features-list li a.active').removeClass('active');
+    $('.main-content .tab-content .tab-pane').removeClass('show active');
+    $('.sidebar .profile-dropdown').addClass('active')
+    $('.features-list li a').on('click', function () {
+      $('.sidebar .profile-dropdown').removeClass('active');
     })
   })
-  
+
   //  Sign in form
   $("#do_not_account").click(function () {
     $("#sign_in").addClass("d-none");
@@ -103,30 +114,118 @@ $(document).ready(function () {
     $(".main-content").toggleClass("active");
     $(this).toggleClass("active");
   });
-});
 
-// Navbar
-jQuery(window).scroll(function () {
-  if (jQuery(window).scrollTop() >= 20) {
-    jQuery("header").addClass("fixed-header");
-  } else {
-    jQuery("header").removeClass("fixed-header");
+  // Select2
+  $(".select").select2({
+    tags: true
+  });
+
+  // Navbar
+  jQuery(window).scroll(function () {
+    if (jQuery(window).scrollTop() >= 20) {
+      jQuery("header").addClass("fixed-header");
+    } else {
+      jQuery("header").removeClass("fixed-header");
+    }
+  });
+
+  // Image Upload Javascript
+  ("use strict");
+  function dragNdrop(event) {
+    var fileName = URL.createObjectURL(event.target.files[0]);
+    var preview = document.getElementById("preview");
+    var previewImg = document.createElement("img");
+    previewImg.setAttribute("src", fileName);
+    preview.innerHTML = "";
+    preview.appendChild(previewImg);
   }
+  function drag() {
+    document.getElementById("uploadFile").parentNode.className = "dragBox";
+  }
+  function drop() {
+    document.getElementById("uploadFile").parentNode.className = "dragBox";
+  }
+
+});
+// Schedule
+document.addEventListener('DOMContentLoaded', function () {
+  var calendarEl = document.getElementById('calendar');
+  var btn = document.getElementsByClassName("fc-filterButton-button");
+  setTimeout(() => {
+    var elms = document.getElementsByClassName('fc-filterButton-button').item(0)
+    elms.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="10.833" height="11.403" viewBox="0 0 10.833 11.403"><path id="filter" d="M6.786,12.528H5.678V6.775L1.687,2.023v-.9H12.52v.894L8.719,6.77V10.6Zm-.347-.76h.033L7.959,10.28V6.5l3.694-4.618H2.564L6.438,6.5Z" transform="translate(-1.687 -1.125)" fill="currentcolor" ></svg>'
+  }, 0);
+
+  var btn = document.getElementsByClassName("fc-menuButton-button");
+  setTimeout(() => {
+    var elms = document.getElementsByClassName('fc-menuButton-button').item(0)
+    elms.innerHTML = '<span class="material-symbols-outlined">list</span>'
+  }, 0);
+
+  var btn3 = document.getElementsByClassName("fc-addButton-button");
+  setTimeout(() => {
+    var elms = document.getElementsByClassName('fc-addButton-button').item(0)
+    elms.innerHTML = '<span class="material-symbols-rounded add_icon">add_circle</span><span>Add New</span>'
+  }, 0);
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    timeZone: 'UTC',
+    themeSystem: 'bootstrap4',
+
+    customButtons: {
+      filterButton: {
+        html: '<svg xmlns="http://www.w3.org/2000/svg" width="10.833" height="11.403" viewBox="0 0 10.833 11.403"><path id="filter" d="M6.786,12.528H5.678V6.775L1.687,2.023v-.9H12.52v.894L8.719,6.77V10.6Zm-.347-.76h.033L7.959,10.28V6.5l3.694-4.618H2.564L6.438,6.5Z" transform="translate(-1.687 -1.125)" fill="currentcolor" /></svg>;'
+      },
+      menuButton: {
+        html: '<span class="material-symbols-outlined">list</span>;'
+      },
+      addButton: {
+        html: '<span class="material-symbols-rounded add_icon">add_circle</span><span>Add New</span>'
+      }
+    },
+    buttonText: {
+      today: 'today',
+      month: 'Monthly',
+      week: 'Weekly',
+      day: 'Daily',
+      list: 'list'
+    },
+    headerToolbar: {
+      left: 'title prev,next filterButton',
+      center: 'timeGridDay,timeGridWeek,dayGridMonth',
+      right: 'menuButton addButton',
+    },
+    initialView: 'timeGridWeek',
+    editable: true,
+    // selectable: true,
+    // weekends: true,
+    // height: 1000,
+    // aspectRatio: 1,
+    weekNumbers: false,
+    expandRows: 4,
+    initialView: 'dayGridMonth',
+    views: {
+      dayGridMonth: {
+        titleFormat: { month: 'long' }
+      },
+    },
+    dayMaxEvents: true, // allow "more" link when too many events
+    events: 'https://fullcalendar.io/api/demo-feeds/events.json'
+  });
+  calendar.render();
 });
 
-// Image Upload Javascript
-("use strict");
-function dragNdrop(event) {
-  var fileName = URL.createObjectURL(event.target.files[0]);
-  var preview = document.getElementById("preview");
-  var previewImg = document.createElement("img");
-  previewImg.setAttribute("src", fileName);
-  preview.innerHTML = "";
-  preview.appendChild(previewImg);
-}
-function drag() {
-  document.getElementById("uploadFile").parentNode.className = "dragBox";
-}
-function drop() {
-  document.getElementById("uploadFile").parentNode.className = "dragBox";
-}
+$(window).on('load', function () {
+  // $('.fc-col-header').children('thead').children('tr').find('.fc-timegrid-axis').append("HELLO");
+  // $('button.fc-timeGridWeek-button').on('click', function () {
+  // });
+  $('.fc-col-header').children('thead').remove();
+})
+
+$(document).ready(function () {
+
+  $(".fc-addButton-button").attr("data-toggle", "modal").attr("href", "#add_shedule_modal");
+});
+
+
+
