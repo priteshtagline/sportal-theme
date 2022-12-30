139b2756c2ec1b67baabe0c0bd2379489a8e6072
YashTagline <yashk.tagline@gmail.com>
2022-12-30 10:48:13 +0530

    schedule module designed

--- js/script.js
+++ js/script.js
@@ -340,7 +340,6 @@ document.addEventListener('DOMContentLoaded', function () {
     var elms = document.getElementsByClassName('fc-filterButton-button').item(0)
     elms.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="10.833" height="11.403" viewBox="0 0 10.833 11.403"><path id="filter" d="M6.786,12.528H5.678V6.775L1.687,2.023v-.9H12.52v.894L8.719,6.77V10.6Zm-.347-.76h.033L7.959,10.28V6.5l3.694-4.618H2.564L6.438,6.5Z" transform="translate(-1.687 -1.125)" fill="currentcolor" ></svg>'
   }, 0);
-  $(".fc-listDay-button").append("<b>Appended text</b>");
 
   var btn2 = document.getElementsByClassName("fc-addButton-button");
   setTimeout(() => {
@@ -503,3 +502,4 @@ $(".tournament-filter").click(function () {
 $(".tournament-filter").click(function () {
   $(".tournament-filter").toggleClass("active");
 });
+
